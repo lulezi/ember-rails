2d09fa7959e4ec1521917d639d2ea65943f38363
Luzi Humm <luzi.humm@elage.ch>
2013-08-25 15:47:19 +0200

    rake dist in ember.js

--- lib/ember_rails.rb
+++ lib/ember_rails.rb
@@ -22,20 +22,34 @@ module Ember
 
       initializer "ember_rails.setup_vendor", :after => "ember_rails.setup", :group => :all do |app, zweite|
         if variant = app.config.ember.variant || ::Rails.env.test?
-          # # test environments should default to development
+          # test environments should default to development
           variant ||= :development
-          # # Copy over the desired ember, ember-data, and handlebars bundled in
-          # # ember-source, ember-data-source, and handlebars-source to a tmp folder. 
-          # tmp_path = app.root.join("tmp/ember-rails")
-          # ext = variant == :production ? ".prod.js" : ".js"
-          # FileUtils.mkdir_p(tmp_path)
-          # FileUtils.cp(::Ember::Source.bundled_path_for("ember#{ext}"), tmp_path.join("ember.js"))
-          # FileUtils.cp(::Ember::Data::Source.bundled_path_for("ember-data#{ext}"), tmp_path.join("ember-data.js"))
-          # app.assets.append_path(tmp_path)
+          # Copy over the desired ember, ember-data, and handlebars bundled in
+          # ember-source, ember-data-source, and handlebars-source to a tmp folder. 
+          tmp_path = app.root.join("tmp/ember-rails")
+          ext = variant == :production ? ".prod.js" : ".js"
+          FileUtils.mkdir_p(tmp_path)
+
+          ember_source_path = `bundle show ember-source`
+          ember_data_source_path = `bundle show ember-data-source`
+          ember_source_path = ember_source_path[0..-2]
+          ember_data_source_path = ember_data_source_path[0..-2]
+
+          FileUtils.cd(ember_source_path) do
+            system "bundle install"
+            system "rm -rf dist"
+            system "bundle exec rake dist"
+            system "pwd"
+            system "ls dist"
+          end
+
+          FileUtils.cp(::Ember::Source.bundled_path_for("ember#{ext}"), tmp_path.join("ember.js"))
+          FileUtils.cp(::Ember::Data::Source.bundled_path_for("ember-data#{ext}"), tmp_path.join("ember-data.js"))
+          app.assets.append_path(tmp_path)
 
           # Make the handlebars.js and handlebars.runtime.js bundled
           # in handlebars-source available.
-          # app.assets.append_path(File.expand_path('../', ::Handlebars::Source.bundled_path))
+          app.assets.append_path(File.expand_path('../', ::Handlebars::Source.bundled_path))
 
           # Allow a local variant override
           ember_path = app.root.join("vendor/assets/ember/#{variant}")
